d921031606f14a95e69ce7e181599f7dbc15cd5e
THEMANNICHOLAS <stingyblue@gmail.com>
2025-04-01 15:30:26 -0400

    Create parent test class

---
+++ src/test/java/BaseClass.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseClass {
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "PATH");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+    @AfterMethod
+    public void afterMethod(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
